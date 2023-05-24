299e6b68f6c747a5215120cab8557f72db166e53
nadia-newaz <nadia.nitu55@gmail.com>
2023-05-24 15:18:42 +0600

    doctors portal

---
+++ src/Component/Default_page/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navigation from '../../Multiple/Navigation/Navigation'
+import Services from '../Services/Services'
+
+
+
+function Home() {
+  return (
+    <>
+    <Navigation></Navigation>
+    <Services/>
+   
+    </>
+  )
+}
+
+export default Home 
---
+++ src/Component/Default_page/Services/Services.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Container } from '@mui/material';
+
+import Flouride from '../../../images/fluoride.png';
+import Cavity from '../../../images/cavity.png';
+import Whitening from '../../../images/whitening.png';
+import Service from '../Service/Service';
+
+const services = [
+  {
+    name: 'Flouride Treatment',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aut maxime accusamus. Nulla, porro tenetur!',
+    img: Flouride
+
+  },
+  {
+    name: 'Cavity Filling',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aut maxime accusamus. Nulla, porro tenetur!',
+    img: Cavity
+
+  },
+  {
+    name: 'Teeth Whitening',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aut maxime accusamus. Nulla, porro tenetur!',
+    img: Whitening
+
+  }
+  
+];
+
+const Services = () => {
+  return (
+    <>
+     <Box sx={{ flexGrow: 1 }}>
+      <Container>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {
+          services.map( service=> <Service
+          key={service.name}
+          service={service}
+          ></Service>
+          )
+        }
+      </Grid>
+      </Container>
+    </Box>
+    
+    </>
+  )
+}
+
+export default Services
