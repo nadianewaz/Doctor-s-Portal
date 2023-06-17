eb68db5938c8c7766e2a4efdf0fb2ef903d44813
nadia-newaz <nadia.nitu55@gmail.com>
2023-06-17 13:49:21 +0600

    firebase

--- src/Component/Loginn/PrivateRoute/PrivateRoute.jsx
+++ src/Component/Loginn/PrivateRoute/PrivateRoute.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route  } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 import { CircularProgress } from '@mui/material';
+import { redirect } from "react-router-dom";
 
 const PrivateRoute = ({ children, ...rest }) => {
     const {user, isLoading } = useAuth();
