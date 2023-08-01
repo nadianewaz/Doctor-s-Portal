28250749b7ea4093e06723dd67cfc76c2820513d
Nitu <aninditaahmed@proton.me>
2023-07-31 20:01:38 -0400

    Adding dateWithFormat to useEffect

--- src/Component/Appointment_Page/Dashboard/Appointments/Appointments.jsx
+++ src/Component/Appointment_Page/Dashboard/Appointments/Appointments.jsx
@@ -25,7 +25,7 @@ const Appointments = ({ date }) => {
             .then( res => res.json())
             .then( data => setAppointments(data));
 
-    }, [date, user.email]);
+    }, [date, user.email, dateWithFormat]);
 
     return (
         <>
